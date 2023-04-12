0686410c63df1ada7cd7a7079109bec2a118af97
YMariya <ymariya2015@gmail.com>
2023-04-12 14:50:26 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

---
+++ sandbox/src/main/java/DisPoint.java
@@ -0,0 +1,11 @@
+public class DisPoint {
+
+    public static void main(String[] args) {
+        double dis1 = new Point().dis1(3,8);
+        double dis2 = new Point().dis2(11,5);
+
+        System.out.println("Расстояние между точками "+ dis1 + " и " + dis2 + " равно" + distance);
+
+    }
+
+}

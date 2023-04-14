48b97c844fe0592fedd253bcae1fec172a166a67
YMariya <ymariya2015@gmail.com>
2023-04-14 13:04:15 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы. Работа над ошибками.

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,21 +1,18 @@
 public class Point {
-    public double x1;
-    public double y1;
-    public double x2;
-    public double y2;
 
-    public double dis1(double x1, double y1) {
-        this.x1 = x1;
-        this.y1 = y1;
-        return this.x1 + this.y1;
+    public static void main(String[] args) {
+
+        double p1 = 8;
+        double p2 = 5;
+
+        System.out.println("Расстояние между точками " + p1 + " и " + p2 + " равно " + distance(p1, p2));
+
     }
-    public double dis2(double x2, double y2) {
-        this.x2 = x2;
-        this.y2 = y2;
-        return this.x2 + this.y2;
-    }
-    public double distance(Point dis1, Point dis2){
-        double d = Math.sqrt((dis2.x2 - dis1.x1) * (dis2.x2 - dis1.x1) + (dis2.y2 - dis1.y1) * (dis2.y2 * dis1.y1));
-        return d;
+
+    public double p1;
+    public double p2;
+
+    public static double distance(double p1, double p2) {
+        return p1 - p2;
     }
 }

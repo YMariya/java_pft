f0ffc6115f021b5fbd47cf60892d6c2df561b3ad
YMariya <ymariya2015@gmail.com>
2023-04-22 23:38:10 +0300

    Задание №2: Работа над ошибками

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,18 +1,23 @@
 public class Point {
 
-    public static void main(String[] args) {
+        public static double x1, y1, x2, y2;
 
-        double p1 = 8;
-        double p2 = 5;
+        public static void main (String[] args) {
+            Point p1 = new Point();
+            Point p2 = new Point();
 
-        System.out.println("Расстояние между точками " + p1 + " и " + p2 + " равно " + distance(p1, p2));
+            p1.x1 = -4;
+            p1.y1 = 10;
+            p2.x2 = 0;
+            p2.y2 = 13;
 
-    }
+            System.out.println("Расстояние между точками = " + distance(p1,p2));
 
-    public double p1;
-    public double p2;
+        }
 
-    public static double distance(double p1, double p2) {
-        return p1 - p2;
-    }
+        public static double distance(Point p1, Point p2){
+
+            return Math.sqrt((p2.x2 - p1.x1) * (p2.x2 - p1.x1) + (p2.y2 - p1.y1)*(p2.y2 - p1.y1));
+
+        }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,24 @@
+
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Alexei");
+
+        Square s = new Square(5); //создать новый обьект
+        //s.l = 5; // начение атрибута
+        System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.area());
+
+        Rectangle r = new Rectangle(4, 5);
+        System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + "=" + +r.area());
+    }
+
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+}

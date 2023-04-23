e9b28bcbe7186df606882a1ad5e33aaed377467c
YMariya <ymariya2015@gmail.com>
2023-04-24 00:36:12 +0300

    Задание №2: Исправленный вариант

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+    public static double x1, y1, x2, y2;
+
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point();
+
+        p1.x1 = -4;
+        p1.y1 = 10;
+
+
+        System.out.println("Расстояние между точками = " + p1.distance(p2));
+
+    }
+
+    private double distance(Point p2) {
+        double dx = this.x2 - x1;
+        double dy = this.y2 - y1;
+        return Math.sqrt(dx * dx + dy * dy);
+
+    }
+
+}
+
+

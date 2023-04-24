17a135b4360de2cbc6f54412f8edac8bbf8a8c52
YMariya <ymariya2015@gmail.com>
2023-04-25 00:01:48 +0300

    Задание №3: testPoint добавлены проверки

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -5,7 +5,21 @@ import org.testng.annotations.Test;
 
 public class PointTests {
     @Test
-    public void testPoint (){
+    public void testPointv1 (){
+
+        Point p1 = new Point();
+        Point p2 = new Point();
+        p1.x1 = 4;
+        p1.y1 = 19;
+        p2.x2 = 5;
+        p2.y2 = 8;
+        Assert.assertEquals(p1.distance(p2),11.045361017187261);
+
+
+    }
+
+    @Test
+    public void testPointv2 (){
 
         Point p1 = new Point();
         Point p2 = new Point();
@@ -13,7 +27,8 @@ public class PointTests {
         p1.y1 = 10;
         p2.x2 = 5;
         p2.y2 = 8;
-        Assert.assertEquals(p1.distance(p2),2.23606797749979);
+        assert p1.distance(p2) == 2.23606797749979;
+
 
     }
 

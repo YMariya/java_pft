7ccc5f53947174222469f633bd57b25bc4bccdff
YMariya <ymariya2015@gmail.com>
2023-05-01 00:53:22 +0300

    Задание 5. Создан новый класс - GroupDeletionTests. Создан общий базовый класс для тестов TestBase, перенесены вспомогательные методы в него (включая инициализацию и завершение). Также сделано для групп, и для контактов:

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -4,37 +4,12 @@ package ru.stqa.pft.addressbook;
 import java.time.Duration;
 
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class ContactCreationTests {
-    private WebDriver wd;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-    private JavascriptExecutor js;
+public class ContactCreationTests extends TestBase {
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-
-        wd = new ChromeDriver();
-        baseUrl = "https://www.google.com/";
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        js = (JavascriptExecutor) wd;
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
 
     @Test
     public void testContactCreation() throws Exception {
@@ -43,47 +18,7 @@ public class ContactCreationTests {
         fillContactForm(new ContactData("Петр", "Иванов", "Москва", "234ff@gmail.com"));
         submitContactCreation();
         returnToHomePage();
-        logout();
-    }
-
-    private void logout() {
-        wd.findElement(By.linkText("Logout")).click();
-    }
-
-    private void returnToHomePage() {
-        wd.findElement(By.linkText("home")).click();
-    }
-
-    private void submitContactCreation() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    }
-
-    private void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
-
-    private void gotoContactPage() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
+        Logout();
     }
 
 

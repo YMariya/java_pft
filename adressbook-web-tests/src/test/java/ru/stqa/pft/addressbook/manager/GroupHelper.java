890be8cff002c22e90d98e04b6c12a3525ec7552
YMariya <ymariya2015@gmail.com>
2023-05-04 23:59:19 +0300

    Лекция 3.10. Реализована проверка и обеспечение выполнения предусловий

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/manager/GroupHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/manager/GroupHelper.java
@@ -50,4 +50,14 @@ public class GroupHelper extends HelperBase {
     }
 
 
+    public void createGroup(GroupData group) {
+       initGroupCreation();
+       fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
 }

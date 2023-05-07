07f067bc83e27bc2aca7bfd0b55ca0007e429f97
YMariya <ymariya2015@gmail.com>
2023-05-08 00:21:05 +0300

    Задание 3. Добавлена проверка

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -11,4 +11,14 @@ public class PointTests {
 
         Assert.assertEquals(p1.distance(p2), 53.85164807134504);
     }
+
+
+
+    @Test
+    public void testPointv2() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(20, 50);
+
+        assert p1.distance(p2) == 53.85164807134504;
+    }
 }

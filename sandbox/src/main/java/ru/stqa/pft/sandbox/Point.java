c425c347f0f14577ed059c42fa812c231483a2a5
YMariya <ymariya2015@gmail.com>
2023-05-08 00:12:31 +0300

    Исправление ответов по замечаниям из заданий 2 и 3.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,32 +1,32 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
+    int x;
+    int у;
 
-    public static double x1, y1, x2, y2;
+    Point(int х, int y) {
+        this.x = х;
+        this.у = y;
+    }
 
-    public Point (double x1, double y1) {
+    double distance(int х, int у) {
+        int dx = this.x - х;
+        int dy = this.у - у;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    double distance(Point p) {
+        return distance(p.x, p.у);
     }
 
 
     public static void main(String[] args) {
-        Point p1 = new Point(1,3);
-        Point p2 = new Point(x2,y2);
-x2 = 4;
-y2 = 5;
-
-
+        Point p1 = new Point(15, 0);
+        Point p2 = new Point(30, 13);
 
         System.out.println("Расстояние между точками = " + p1.distance(p2));
 
     }
-      public double distance( Point p2) {
-        double dx = this.x2 - x1;
-        double dy = this.y2 - y1;
-        return Math.sqrt(dx * dx + dy * dy);
-
-    }
-
-
 }
 
 
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -5,26 +5,10 @@ import org.testng.annotations.Test;
 
 public class PointTests {
     @Test
-    public void testPointv1 (){
+    public void testPointv1() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(20, 50);
 
-        Point p1 = new Point(4,19);
-
-        Point p2 = new Point(Point.x2,Point.y2);
-        Point.x2 = 4;
-        Point.y2 = 5;
-        Assert.assertEquals(p1.distance(p2),11.045361017187261);
-
-
+        Assert.assertEquals(p1.distance(p2), 53.85164807134504);
     }
-
-    @Test
-    public void testPointv2 (){
-
-        Point p1 = new Point(1,5);
-        Point p2 = new Point(3,9);
-        assert p1.distance(p2) == 2.23606797749979;
-
-
-    }
-
 }

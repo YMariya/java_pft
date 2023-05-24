9553e795de0e2b48b592528c4dfdd343203c0496
YMariya <ymariya2015@gmail.com>
2023-05-24 16:20:48 +0300

    Лекция 4.9

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -23,13 +24,7 @@ public class ContactCreationTests extends TestBase {
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(),before.size() + 1);
 
-        int max = 0;
-        for (ContactData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-            contact.setId(max);
+        contact.setId(after.stream().max((Comparator.comparingInt(ContactData::getId))).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,13 +22,13 @@ public class GroupCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size() + 1);
 
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+//        int max = 0;
+//        for (GroupData g : after) {
+//            if (g.getId() > max) {
+//                max = g.getId();
+//            }
+//        }
+        group.setId(after.stream().max((Comparator.comparingInt(GroupData::getId))).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 

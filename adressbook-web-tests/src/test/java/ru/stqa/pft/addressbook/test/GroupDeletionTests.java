1de44ba8d632d353be7d3c000ec0319733f9a1df
YMariya <ymariya2015@gmail.com>
2023-05-25 00:11:19 +0300

    Лекция 5.2. Улучшение кода тестов: Groups

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTests.java
@@ -1,20 +1,23 @@
 package ru.stqa.pft.addressbook.test;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
-
-    @Test
-    public void testGroupDeletion() throws Exception {
+    @BeforeMethod
+    public  void ensurePrecondition() {
         app.getNavigationHelper().gotoGroupPage();
 
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+    }
+    @Test
+    public void testGroupDeletion() throws Exception {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroups(before.size() - 1);
         app.getGroupHelper().deleteSelectGroups();

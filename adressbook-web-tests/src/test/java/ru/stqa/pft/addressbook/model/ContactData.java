6b00fdc76ab22b69ab0d02e30257a0cb4f758d5e
YMariya <ymariya2015@gmail.com>
2023-05-24 16:56:17 +0300

    Задание 9. Реализованы проверки путем сравнения списков. Исправление проверок

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -59,6 +59,7 @@ public class ContactData {
         return "ContactData{" +
                 "id=" + id +
                 ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
                 '}';
     }
 
@@ -70,13 +71,15 @@ public class ContactData {
         ContactData that = (ContactData) o;
 
         if (id != that.id) return false;
-        return firstname != null ? firstname.equals(that.firstname) : that.firstname == null;
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
     }
 
     @Override
     public int hashCode() {
         int result = id;
         result = 31 * result + (firstname != null ? firstname.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
         return result;
     }
 }

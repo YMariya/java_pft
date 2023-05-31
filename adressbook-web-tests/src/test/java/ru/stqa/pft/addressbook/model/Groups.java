eb880f9c52055b723361e7a7529046816ec7c717
YMariya <ymariya2015@gmail.com>
2023-06-01 01:32:58 +0300

    Лекция 5.6. Hamcrest: улучшение внешнего вида проверок. Groups Исправление опечатки

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class Groups extends ForwardingSet<GroupData> {
 
-private Set<GroupData> delegate
+private Set<GroupData> delegate;
 
     public Groups(Groups groups) {
     this.delegate = new HashSet<>(groups.delegate);
